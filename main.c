294f741e81308d985dd7fd8d604ccbfd4a186af0
gaeul <gaeul0627@mju.ac.kr>
2019-11-01 17:40:29 +0900

    except 3

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "myProject.h"
+int main ()
+{
+	printf("Chief name is gaeul\n");
+	printMyName1();
+	printMyName2();
+	printMyName3();
+	return 0;
+}
---
+++ myProject.h
@@ -0,0 +1,6 @@
+#ifndef _MYPROJECT_H_
+#define _MYPROJECT_H_
+void printMyname1(void);
+void printMyname2(void);
+void printMyname3(void);
+#endif
