439b0376c83a636d0cf56c2ca5e203c64d2f7d84
suhyun <bear5585@mju.ac.kr>
2019-12-03 22:52:47 +0900

    buzzer

---
+++ buzzerlib/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <dirent.h>
+#include "buzzer.h"
+
+#define MAX_SCALE_STEP		8
+
+
+int main(int argc , char **argv) 
+{ 
+
+if (buzzerInit() == 0)
+	{
+		printf ("Buzzer init Failed!\r\n");
+		return 0;
+    }
+
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(0);
+    buzzerStopSong();    
+    usleep(333333);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(333333);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(700000);
+
+    buzzerPlaySong(2);
+    buzzerStopSong();
+    usleep(700000);
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(2);
+    buzzerStopSong();
+    usleep(700000);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(700000);
+
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(2);
+    buzzerStopSong();
+    usleep(700000);
+
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(4);
+    buzzerStopSong();
+    usleep(500000);
+    buzzerPlaySong(2);
+    buzzerStopSong();
+    usleep(700000);
+
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(700000);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(700000);
+    buzzerPlaySong(0);
+    buzzerStopSong();
+    usleep(700000);		
+
+	buzzerExit();
+
+	return 1;
+
+
+}
+
