b263c581304044e46d536561bb89f6fbf9f19eed
Yongbeom Huh <huhyb77@mju.ac.kr>
2019-12-13 21:50:54 +0900

    hide fileopen

--- main/temp.c
+++ main/temp.c
@@ -11,12 +11,16 @@
 #include <linux/spi/spidev.h>
 #include "temp.h"
 char gbuf[10];
-int spi_init(char filename[40])
-{
+//char * spi_read_lm74(int file)
+int temp(void){
+	char *buffer;
 	int file;
+	
+
+	
 	__u8 mode, lsb, bits;
 	__u32 speed=20000;
-		if ((file = open(filename,O_RDWR)) < 0) {
+		if ((file = open("/dev/spidev1.0",O_RDWR)) < 0) {
 			printf("Failed to open the bus."); printf ("ErrorType:%d\r\n",errno); exit(1); }
 			
 			//possible modes: mode |= SPI_LOOP; mode |= SPI_CPHA; mode |= SPI_CPOL; mode |= SPI_LSB_FIRST; mode |= SPI_CS_HIGH;
@@ -29,18 +33,24 @@ int spi_init(char filename[40])
 				{ perror("SPI rd_lsb_fist"); return 0; }
 			if (ioctl(file, SPI_IOC_RD_BITS_PER_WORD, &bits) < 0)
 				{perror("SPI bits_per_word"); return 0; }
-			printf("%s: spi mode %d, %d bits %sper word, %d Hz max\n",filename, mode, bits, lsb ? "(lsb first) " : "", speed);
-			return file;
-}
-char * spi_read_lm74(int file)
-{
-		int len;
-		memset(gbuf, 0, sizeof( gbuf));
-		len = read(file, gbuf, 2); //Read two byte!
-		if (len !=2)
-		{
-			perror("read error");
-			return NULL;
-		}
-		return gbuf;
+	
+//			buffer = (char *)spi_read_lm74(file); //already Read,
+
+		 int len;
+                memset(gbuf, 0, sizeof( gbuf));
+                len = read(file, gbuf, 2); //Read two byte!
+                if (len !=2)
+                {
+                        perror("read error");
+                        return 0;
+                }
+                buffer = gbuf;
+		int value = (buffer[0] << 8 ) | buffer[1];		//Big Endian->Little Endian
+		if ( value < 0 )
+	{	 
+		value = (int)(value^(-1)); 
+	}
+	value >>= 7; //1의 자리에서 버림	
+	close(file);
+	return value;
 }
--- main/temp.h
+++ main/temp.h
@@ -1,7 +1,6 @@
 #ifndef _TEMP_H_
 #define _TEMP_H_
-int spi_init(char filename[40]);
-char * spi_read_lm74(int file);
+int temp(void);
 #endif
 
 
